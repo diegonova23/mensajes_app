02c4ef743b35aea65ffdb13381dc759b8e0edd16
diegonova23 <dorellana@groupnova.com.gt>
2023-03-10 12:20:31 -0600

    first commit for mensajes_app

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import java.sql.Connection;
+
+public class Main {
+    public static void main(String[] args) {
+        Conexion connection = new Conexion();
+        try(Connection cnx = connection.get_connection()) {
+
+        } catch(Exception e){
+            System.out.println(e);
+        }
+    }
+}
